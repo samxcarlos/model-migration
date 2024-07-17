6a8383e3ccc099cfa8f91e18eb6f846b15509a32
Samuel Carlos <carlosxsamuel7262@gmail.com>
2024-07-17 11:06:32 +0800

    dummy data

---
+++ app/Models/Student.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $table = "students";
+    protected $fillable = [
+        'fname',
+        'lname',
+        'email',
+        'phone',
+        'address',
+        'city',
+        'province',
+        'zip',
+        'birthdate',
+    ];
+
+    //protected $guarded = ['zip'];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Student;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        Student::factory()->count(30)->create();
 
         User::factory()->create([
             'name' => 'Test User',
